5cced186e90569ef4e4bcd0fe96f1dbfa85fff83
Abhishek Kumar <abhisheksingnbr@gmail.com>
2021-08-24 09:25:22 +0530

    Update Prim’s Algorithm.cpp

--- Prim’s Algorithm.cpp
+++ Prim’s Algorithm.cpp
@@ -17,6 +17,11 @@ example:
         0 2 1
     
 Output: 4
+  
+Solution approach:-
+    starts with an empty spanning tree. The idea is to maintain two sets of vertices. The first set contains the vertices already included in the MST, 
+    the other set contains the vertices not yet included. At every step, it considers all the edges that connect the two sets, 
+    and picks the minimum weight edge from these edges. After picking the edge, it moves the other endpoint of the edge to the set containing MST. 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 int PrimsMinimumSpanningTree(int V, vector<vector<int>> edges)
